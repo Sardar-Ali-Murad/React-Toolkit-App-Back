3efec88cae37d52a67e856a40cb878ed410cd2f9
your_username <sardaralimuradali4@gmail.com>
2023-01-30 23:46:13 +0500

    resolving cookie

--- utils/attachCookie.js
+++ utils/attachCookie.js
@@ -1,11 +1,16 @@
 const attachCookie = ({ res, token }) => {
-  const oneDay = 1000 * 60 * 60 * 24;
+  // const oneDay = 1000 * 60 * 60 * 24;
 
-  res.cookie('token', token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
-  });
+  // res.cookie('token', token, {
+  //   httpOnly: true,
+  //   expires: new Date(Date.now() + oneDay),
+  //   secure: process.env.NODE_ENV === 'production',
+  // });
+  
+  var randomNumber=Math.random().toString();
+  randomNumber=randomNumber.substring(2,randomNumber.length);
+  res.cookie('token',token, { maxAge: 900000, httpOnly: true });
+  
 };
 
 export default attachCookie;
