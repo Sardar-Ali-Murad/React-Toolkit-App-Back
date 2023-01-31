f67a69d7cbe1a7b32219166f0d1f1b3d427c6115
your_username <sardaralimuradali4@gmail.com>
2023-02-01 02:29:21 +0500

    xzsx

--- index.js
+++ index.js
@@ -70,6 +70,24 @@ app.use(cors({
 }))
 
 
+const sessionConfig = {
+  secret: 'MYSECRET',
+  name: 'appName',
+  resave: false,
+  saveUninitialized: false,
+  store: store,
+  cookie : {
+    sameSite: 'strict', // THIS is the config you are looing for.
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sessionConfig.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionConfig));
+
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/property', Property);
